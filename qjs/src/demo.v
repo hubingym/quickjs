ba186768287008075316deeb94f4d9a43eb7a7ee
hubing <365356015@qq.com>
2019-08-13 08:26:48 +0800

    initial

--- qjs/src/demo.v
+++ qjs/src/demo.v
@@ -14,23 +14,41 @@ fn fib(n int) int {
 
 fn js_fib(p_ctx *C.JSContext, this_val C.JSValueConst, argc int, argv *C.JSValueConst) C.JSValue {
     n := 0
-    if C.JS_ToInt32(p_ctx, &n, argv[0]) {
+    if C.JS_ToInt32(p_ctx, &n, argv[0]) != 0 {
         return C.JS_MKVAL(C.JS_TAG_EXCEPTION, 0)
     }
     res := fib(n)
     return C.JS_NewInt32(p_ctx, res)
 }
 
-fn init_module_demo(ctx quickjs.Context) {
-    mut m := ctx.add_module('demo')
+// fn open_counter(p_ctx *C.JSContext, this_val C.JSValueConst, argc int, argv *C.JSValueConst) C.JSValue {
+//     n := 0
+//     if C.JS_ToInt32(p_ctx, &n, argv[0]) != 0 {
+//         return C.JS_MKVAL(C.JS_TAG_EXCEPTION, 0)
+//     }
+//     obj := C.JS_NewObjectClass(p_ctx, class_id)
+//     return obj
+// }
+//
+// fn counter_update(p_ctx *C.JSContext, this_val C.JSValueConst, argc int, argv *C.JSValueConst) C.JSValue {
+//     return C.JS_MKVAL(C.JS_TAG_UNDEFINED, 0)
+// }
+
+fn demo_init(p_ctx *C.JSContext, p_m *C.JSModuleDef) {
+    ctx := quickjs.make_context(p_ctx)
+    m := quickjs.make_module(p_ctx, p_m)
     m.new_cfunction('fib', 1, &js_fib)
-    m.new_jsval('global', ctx.get_global_bject())
     m.new_int32('O_WRONLY', C.O_WRONLY)
     m.new_double('pi', 3.14)
     m.new_string('platform', 'win32')
 
     obj1 := ctx.new_object()
-    ctx.define_property_double(obj1, 'pi', 3.14)
-    ctx.define_property_cfunc(obj1, 'fib', 1, &js_fib)
+    ctx.set_property_double(obj1, 'pi', 3.14)
+    ctx.set_property_cfunc(obj1, 'fib', 1, &js_fib)
     m.new_jsval('obj1', obj1)
+}
+
+fn init_module_demo(ctx quickjs.Context) {
+    m := ctx.add_module('demo', &demo_init)
+    m.add_export_list(['fib', 'O_WRONLY', 'pi', 'platform', 'obj1'])
 }
--- quickjs.v
+++ quickjs.v
@@ -20,6 +20,11 @@ struct C.JSContext {
 struct C.JSModuleDef {
 }
 
+// struct C.JSClassDef {
+//     class_name byteptr
+//     finalizer voidptr
+// }
+
 struct C.JSClassDef {
 }
 
@@ -29,28 +34,26 @@ struct C.JSValue {
 struct C.JSValueConst {
 }
 
+fn C.JS_ToInt32(*C.JSContext, *u32, C.JSValueConst) int
 fn C.JS_GetRuntime(*C.JSContext) *C.JSRuntime
 fn C.JS_NewClassID(*u32) u32
 fn C.JS_NewClass(*C.JSRuntime, u32, *C.JSClassDef) int
 fn C.JS_SetClassProto(*C.JSContext, u32, C.JSValue)
-fn C.JS_DefinePropertyValueStr(*C.JSContext, C.JSValueConst, byteptr, C.JSValue, int) int
+fn C.JS_SetPropertyStr(*C.JSContext, C.JSValueConst, byteptr, C.JSValue) int
+fn C.JS_NewObjectClass(*C.JSContext, u32) C.JSValue
 fn C.JS_NewObject(*C.JSContext) C.JSValue
+fn C.JS_NewArray(*C.JSContext) C.JSValue
 fn C.JS_GetGlobalObject(*C.JSContext) C.JSValue
+fn C.JS_DupValue(*C.JSContext, C.JSValueConst) C.JSValue
 fn C.JS_NewCFunction(*C.JSContext, voidptr, byteptr, int) C.JSValue
 fn C.JS_NewBool(*C.JSContext, int) C.JSValue
 fn C.JS_NewInt32(*C.JSContext, i32) C.JSValue
 fn C.JS_NewFloat64(*C.JSContext, f64) C.JSValue
-fn C.__JS_NewFloat64(*C.JSContext, f64) C.JSValue
 fn C.JS_NewString(*C.JSContext, byteptr) C.JSValue
 fn C.JS_AddModuleExport(*C.JSContext, *C.JSModuleDef, byteptr) int
 fn C.JS_SetModuleExport(*C.JSContext, *C.JSModuleDef, byteptr, C.JSValue) int
 
 fn todo_remove(){}
-
-struct Opaque {
-pub: mut:
-    modules []voidptr
-}
 
 struct Runtime {
 pub: mut:
@@ -76,7 +79,7 @@ pub fn (rt mut Runtime) free() {
 }
 
 struct Context {
-pub: mut:
+mut:
     p_ctx *C.JSContext
 }
 
@@ -88,8 +91,13 @@ pub fn new_context(rt Runtime) Context {
     ctx := Context {
         p_ctx: p_ctx
     }
-    opaque := &Opaque{}
-    C.JS_SetContextOpaque(p_ctx, opaque)
+    return ctx
+}
+
+pub fn make_context(p_ctx *C.JSContext) Context {
+    ctx := Context {
+        p_ctx: p_ctx
+    }
     return ctx
 }
 
@@ -115,36 +123,40 @@ pub fn (ctx Context) loop() {
     C.js_std_loop(ctx.p_ctx)
 }
 
-pub fn (ctx Context) define_property_value_str(this_obj C.JSValue, prop string, val C.JSValue) int {
-    return C.JS_DefinePropertyValueStr(ctx.p_ctx, this_obj, prop.str, val, JS_PROP_C_W_E)
-}
-
-pub fn (ctx Context) define_property_cfunc(this_obj C.JSValue, prop string, length int, func voidptr) int {
+pub fn (ctx Context) set_property_cfunc(this_obj C.JSValue, prop string, length int, func voidptr) int {
     val := C.JS_NewCFunction(ctx.p_ctx, func, prop.str, length)
-    return ctx.define_property_value_str(this_obj, prop, val)
+    return C.JS_SetPropertyStr(ctx.p_ctx, this_obj, prop.str, val)
 }
 
-pub fn (ctx Context) define_property_bool(this_obj C.JSValue, prop string, value bool) int {
+pub fn (ctx Context) set_property_bool(this_obj C.JSValue, prop string, value bool) int {
     val := C.JS_NewBool(ctx.p_ctx, value)
-    return ctx.define_property_value_str(this_obj, prop, val)
+    return C.JS_SetPropertyStr(ctx.p_ctx, this_obj, prop.str, val)
 }
 
-pub fn (ctx Context) define_property_int32(this_obj C.JSValue, prop string, value int) int {
+pub fn (ctx Context) set_property_int32(this_obj C.JSValue, prop string, value int) int {
     val := C.JS_NewInt32(ctx.p_ctx, value)
-    return ctx.define_property_value_str(this_obj, prop, val)
+    return C.JS_SetPropertyStr(ctx.p_ctx, this_obj, prop.str, val)
 }
 
-pub fn (ctx Context) define_property_double(this_obj C.JSValue, prop string, value f64) int {
+pub fn (ctx Context) set_property_double(this_obj C.JSValue, prop string, value f64) int {
     val := C.JS_NewFloat64(ctx.p_ctx, value)
-    return ctx.define_property_value_str(this_obj, prop, val)
+    return C.JS_SetPropertyStr(ctx.p_ctx, this_obj, prop.str, val)
 }
 
-pub fn (ctx Context) define_property_string(this_obj C.JSValue, prop string, value string) int {
+pub fn (ctx Context) set_property_string(this_obj C.JSValue, prop string, value string) int {
     val := C.JS_NewString(ctx.p_ctx, value.str)
-    return ctx.define_property_value_str(this_obj, prop, val)
+    return C.JS_SetPropertyStr(ctx.p_ctx, this_obj, prop.str, val)
 }
 
-pub fn (ctx Context) get_global_bject() C.JSValue {
+pub fn (ctx Context) dup_value(val C.JSValue) C.JSValue {
+    return C.JS_DupValue(ctx.p_ctx, val)
+}
+
+pub fn (ctx Context) free_value(val C.JSValue) {
+    C.JS_FreeValue(ctx.p_ctx, val)
+}
+
+pub fn (ctx Context) get_global_object() C.JSValue {
     return C.JS_GetGlobalObject(ctx.p_ctx)
 }
 
@@ -152,85 +164,42 @@ pub fn (ctx Context) new_object() C.JSValue {
     return C.JS_NewObject(ctx.p_ctx)
 }
 
-pub fn (ctx Context) add_module(name string) *Module {
+pub fn (ctx Context) new_array() C.JSValue {
+    return C.JS_NewArray(ctx.p_ctx)
+}
+
+pub fn (ctx Context) add_module(name string, init_func voidptr) Module {
     p_ctx := ctx.p_ctx
-    m := new_module(p_ctx, name, &module_init_func)
-    mut opaque := (*Opaque)(C.JS_GetContextOpaque(p_ctx))
-    opaque.modules << (*void)(m)
+    m := new_module(p_ctx, name, init_func)
     return m
-}
-
-fn module_init_func(p_ctx *C.JSContext, p_m *C.JSModuleDef) {
-    mut m := find_module(p_ctx, p_m)
-    m.inner_init()
-}
-
-pub fn find_module(p_ctx *C.JSContext, p_m *C.JSModuleDef) *Module {
-    opaque := (*Opaque)(C.JS_GetContextOpaque(p_ctx))
-    for v in opaque.modules {
-        m := (*Module)(v)
-        if m.p_m == p_m {
-            return m
-        }
-    }
-    panic('quickjs: cannot find JS CModule')
-    return &Module{}
-}
-
-struct CFuncDef {
-    name string
-    length int
-    func voidptr
-}
-
-pub fn new_cfunc_def(name string, length int, func voidptr) CFuncDef {
-    return CFuncDef {
-        name: name
-        length: length
-        func: func
-    }
-}
-
-struct JsValueDef {
-    name string
-    val C.JSValue
-}
-
-pub fn new_jsvalue_def(name string, val C.JSValue) JsValueDef {
-    return JsValueDef {
-        name: name
-        val: val
-    }
 }
 
 struct Module {
     p_ctx *C.JSContext
     p_m *C.JSModuleDef
-    funcs []CFuncDef
-    vals []JsValueDef
-    bool_map map[string]bool
-    i32_map map[string]int
-    f64_map map[string]f64
-    str_map map[string]string
 }
 
-fn new_module(p_ctx *C.JSContext, name string, init_func voidptr) *Module {
+fn new_module(p_ctx *C.JSContext, name string, init_func voidptr) Module {
     p_m := C.JS_NewCModule(p_ctx, name.str, init_func)
     if !p_m {
         panic('quickjs: cannot allocate JS CModule')
     }
-    m := &Module {
+    m := Module {
         p_ctx: p_ctx
         p_m: p_m
-        bool_map: map[string]bool{}
-        i32_map: map[string]int{}
-        f64_map: map[string]f64{}
-        str_map: map[string]string{}
     }
     return m
 }
 
-fn (m mut Module) add_export_item(name string) int {
+pub fn make_module(p_ctx *C.JSContext, p_m *C.JSModuleDef) Module {
+    m := Module {
+        p_ctx: p_ctx
+        p_m: p_m
+    }
+    return m
+}
+
+pub fn (m Module) add_export_item(name string) int {
     res := C.JS_AddModuleExport(m.p_ctx, m.p_m, name.str)
     if res != 0 {
         panic('quickjs: add_export_item failed')
@@ -238,7 +207,7 @@ fn (m mut Module) add_export_item(name string) int {
     return res
 }
 
-fn (m mut Module) add_export_list(names []string) int {
+pub fn (m Module) add_export_list(names []string) int {
     for name in names {
         if m.add_export_item(name) != 0 {
             return -1
@@ -247,72 +216,47 @@ fn (m mut Module) add_export_list(names []string) int {
     return 0
 }
 
-fn (m mut Module) set_export_item(name string, val C.JSValue) {
+pub fn (m Module) set_export_item(name string, val C.JSValue) {
     res := C.JS_SetModuleExport(m.p_ctx, m.p_m, name.str, val)
     if res != 0 {
         panic('quickjs: set_export_item failed')
     }
 }
 
-fn (m mut Module) inner_init() {
-    for def in m.funcs {
-        val := C.JS_NewCFunction(m.p_ctx, def.func, def.name.str, def.length)
-        m.set_export_item(def.name, val)
-    }
-
-    for def in m.vals {
-        m.set_export_item(def.name, def.val)
-    }
-
-    for name, value in m.bool_map {
-        val := C.JS_NewBool(m.p_ctx, value)
-        m.set_export_item(name, val)
-    }
-
-    for name, value in m.i32_map {
-        val := C.JS_NewInt32(m.p_ctx, value)
-        m.set_export_item(name, val)
-    }
-
-    for name, value in m.f64_map {
-        val := C.__JS_NewFloat64(m.p_ctx, value)
-        m.set_export_item(name, val)
-    }
-
-    for name, value in m.str_map {
-        val := C.JS_NewString(m.p_ctx, value.str)
-        m.set_export_item(name, val)
-    }
+pub fn (m Module) new_cfunction(name string, length int, func voidptr) {
+    val := C.JS_NewCFunction(m.p_ctx, func, name.str, length)
+    m.set_export_item(name, val)
 }
 
-pub fn (m mut Module) new_cfunction(name string, length int, func voidptr) {
-    m.add_export_item(name)
-    def := new_cfunc_def(name, length, func)
-    m.funcs << def
+pub fn (m Module) new_jsval(name string, val C.JSValue) {
+    m.set_export_item(name, val)
 }
 
-pub fn (m mut Module) new_jsval(name string, val C.JSValue) {
-    m.add_export_item(name)
-    def := new_jsvalue_def(name, val)
-    m.vals << def
+pub fn (m Module) new_bool(name string, value bool) {
+    val := C.JS_NewBool(m.p_ctx, value)
+    m.set_export_item(name, val)
 }
 
-pub fn (m mut Module) new_bool(name string, value bool) {
-    m.add_export_item(name)
-    m.bool_map[name] = value
+pub fn (m Module) new_int32(name string, value int) {
+    val := C.JS_NewInt32(m.p_ctx, value)
+    m.set_export_item(name, val)
 }
 
-pub fn (m mut Module) new_int32(name string, value int) {
-    m.add_export_item(name)
-    m.i32_map[name] = value
+pub fn (m Module) new_double(name string, value f64) {
+    val := C.JS_NewFloat64(m.p_ctx, value)
+    m.set_export_item(name, val)
 }
 
-pub fn (m mut Module) new_double(name string, value f64) {
-    m.add_export_item(name)
-    m.f64_map[name] = value
+pub fn (m Module) new_string(name string, value string) {
+    val := C.JS_NewString(m.p_ctx, value.str)
+    m.set_export_item(name, val)
 }
 
-pub fn (m2 mut Module) new_string(name string, value string) {
-    m2.add_export_item(name)
-    m2.str_map[name] = value
-}
+// pub fn (m Module) new_class(class_def *C.JSClassDef, proto C.JSValue) {
+//     class_id := u32(0)
+//     p_ctx := m.p_ctx
+//     p_rt := C.JS_GetRuntime(p_ctx)
+//     C.JS_NewClassID(&class_id)
+//     C.JS_NewClass(p_rt, class_def, class_def)
+//     C.JS_SetClassProto(p_ctx, class_id, proto)
+// }
